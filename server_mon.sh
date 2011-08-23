a03b6e682a300ad501e0fd7f7ac6c9d422c8c600
Anton de Winter <adewinter@dimagi.com>
2011-08-23 18:32:41 -0400

    modded server_mon to output an XML file according to the custom HTTP pingdom check

--- server_mon.sh
+++ server_mon.sh
@@ -2,41 +2,65 @@
 
 SUPERV_PROCS=("$@")
 SUPERV_NUM_PROCS=$#
+#want to create an additional log?
+LOG_ENABLED=true
+LOG_FILE_LOCATION=/home/aremind/www/production/log/pingdom_log.log
+#folder where you'd like to host the pid file for this process monitor
+PINGDOM_FILE_LOC=/home/aremind/www/production/code_root/static_files/pingdom
+#file name of the PID file. Can be anything
+PINGDOM_FILE_NAME=aremind_celerybeat.pid
+DEBUG=True
 
+PROC_STATUS=OK
+######################################
+#Example check file contents. Must follow this structure!  Using the response time for number of processes:
+#
+#<pingdom_http_custom_check>
+#        <status>OK</status>
+#        <response_time>96.777</response_time>
+#</pingdom_http_custom_check>
+#
+#######################################
 
 for var in $@
 do
-	echo $var 'ARG ECHOED'
+	#the meat of the script.
+	PROC_STATUS_TEXT=`/usr/bin/supervisorctl status | grep $var`
+	PROC_RUNNING=`/usr/bin/supervisorctl status | grep $var | grep RUNNING`
+	if [ ! -z "$PROC_RUNNING" ]
+	then
+		IS_RUNNING = "False"
+	else
+		IS_RUNNING = "True"
+	fi
+	
+	if [ $DEBUG ]
+	do
+		echo "--------------------------------"
+		echo "For proc: $var::"
+		echo "`date`: $PROC_STATUS_TEXT" 	
+		echo "IS PROC RUNNING?: $IS_RUNNING"
+		echo "--------------------------------"
+	fi
+		
+	if [ $LOG_ENABLED ]
+	then
+		echo "`date`: $PROC_STATUS_TEXT" >> $LOG_FILE_LOCATION
+	fi
+	if [ ! -z "$PROC_RUNNING" ]
+	then
+		PROC_STATUS=FAIL
+	fi
 done
-echo $SUPERV_PROCS '-> ARGNAMES'
-echo $SUPERV_NUM_PROCS '-> NUMBER OF ARGUMENTS'
 
-#This is the process string line.  Run 'sudo supervisorctl status' to see some other examples.
-#SUPERV_PROC_NAME='aremind-production:aremind-production-celerybeat'
-#HERE=./
-#folder where you'd like to host the pid file for this process monitor
-#PINGDOM_FILE_LOC=/home/aremind/www/production/code_root/static_files/pingdom
-#file name of the PID file. Can be anything
-#PINGDOM_FILE_NAME=aremind_celerybeat.pid
+echo "<pingdom_http_custom_check><status>" > $PINGDOM_FILE_LOC/$PINGDOM_FILE_NAME
+echo "$PROC_STATUS</status><response_time>96.777</response_time></pingdom_http_custom_check>" >> $PINGDOM_FILE_LOC/$PINGDOM_FILE_NAME
 
-#want to create an additional log?
-#LOG_ENABLED=true
-#LOG_FILE_LOCATION=/home/aremind/www/production/log/pingdom_log.log
-
-#the meat of the script.
-#PROC_STATUS=`/usr/bin/supervisorctl status | grep $SUPERV_PROC_NAME`
-#PROC_RUNNING=`/usr/bin/supervisorctl status | grep $SUPERV_PROC_NAME | grep RUNNING`
-
-#if [ $LOG_ENABLED ]
-#then
-#	echo "`date` LOGGING TO LOG: $PROC_STATUS"
-#	echo "PROCESS UP: $PROC_STATUS" >> $LOG_FILE_LOCATION
-#fi
-#
-#if [ ! -z "$PROC_RUNNING" ]
-#then
-#	touch $PINGDOM_FILE_LOC/$PINGDOM_FILE_NAME
-#else
-#	rm $PINGDOM_FILE_LOC/$PINGDOM_FILE_NAME
-#fi
+if [ $DEBUG ]
+then
+	echo "outputting PINGDOM FILE:"
+	echo "---------------------STARTFILE:"
+	cat $PINGDOM_FILE_LOC/$PINGDOM_FILE_NAME
+	echo ":ENDFILE------------------------"
+fi
 
