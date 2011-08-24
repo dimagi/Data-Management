d81c7639ea114b1bcb01bc54ecf7695556b775b5
Anton de Winter <adewinter@dimagi.com>
2011-08-24 08:55:22 -0700

    Converted server_mon.sh to output pingdom compatible xml

--- server_mon.sh
+++ server_mon.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-SUPERV_PROCS=("$@")
+#SUPERV_PROCS=("$@"
 SUPERV_NUM_PROCS=$#
 #want to create an additional log?
 LOG_ENABLED=true
-LOG_FILE_LOCATION=/home/aremind/www/production/log/pingdom_log.log
+LOG_FILE_LOCATION=/home/adewinter/pingdom_server_mon_log.log
+#LOG_FILE_LOCATION=/home/aremind/www/production/log/pingdom_log.log
 #folder where you'd like to host the pid file for this process monitor
-PINGDOM_FILE_LOC=/home/aremind/www/production/code_root/static_files/pingdom
+PINGDOM_FILE_LOC=/home/adewinter/
+#PINGDOM_FILE_LOC=/home/aremind/www/production/code_root/static_files/pingdom
 #file name of the PID file. Can be anything
 PINGDOM_FILE_NAME=aremind_celerybeat.pid
 DEBUG=True
@@ -22,39 +24,48 @@ PROC_STATUS=OK
 #
 #######################################
 
+if [ $DEBUG ]
+then
+	echo DEBUG IS ENABLED.
+fi
+
+
 for var in $@
 do
 	#the meat of the script.
 	PROC_STATUS_TEXT=`/usr/bin/supervisorctl status | grep $var`
 	PROC_RUNNING=`/usr/bin/supervisorctl status | grep $var | grep RUNNING`
-	if [ ! -z "$PROC_RUNNING" ]
+	if [ -z "$PROC_RUNNING" ]
 	then
-		IS_RUNNING = "False"
+		IS_RUNNING="False"
+		PROC_STATUS=FAIL
 	else
-		IS_RUNNING = "True"
+		IS_RUNNING="True"
 	fi
 	
 	if [ $DEBUG ]
-	do
+	then
 		echo "--------------------------------"
 		echo "For proc: $var::"
-		echo "`date`: $PROC_STATUS_TEXT" 	
+		echo `date`": $PROC_STATUS_TEXT"
 		echo "IS PROC RUNNING?: $IS_RUNNING"
+		echo "PROC_RUNNING: $PROC_RUNNING"
 		echo "--------------------------------"
+		echo 
+		echo
 	fi
 		
 	if [ $LOG_ENABLED ]
 	then
-		echo "`date`: $PROC_STATUS_TEXT" >> $LOG_FILE_LOCATION
-	fi
-	if [ ! -z "$PROC_RUNNING" ]
-	then
-		PROC_STATUS=FAIL
+		echo `date`": $PROC_STATUS_TEXT" >> $LOG_FILE_LOCATION
+		if [ $PROC_STATUS == "FAIL" ]
+		then
+			echo `date`": PROCESS $var IS DOWN"
+		fi
 	fi
 done
 
-echo "<pingdom_http_custom_check><status>" > $PINGDOM_FILE_LOC/$PINGDOM_FILE_NAME
-echo "$PROC_STATUS</status><response_time>96.777</response_time></pingdom_http_custom_check>" >> $PINGDOM_FILE_LOC/$PINGDOM_FILE_NAME
+echo "<pingdom_http_custom_check><status>$PROC_STATUS</status><response_time>$#</response_time></pingdom_http_custom_check>" > $PINGDOM_FILE_LOC/$PINGDOM_FILE_NAME
 
 if [ $DEBUG ]
 then
---
+++ server_mon_wrapper.sh
@@ -0,0 +1,2 @@
+./server_mon.sh aremind-production:aremind-production-celerybeat "aremind-production:aremind-production-celeryd" foo
+
